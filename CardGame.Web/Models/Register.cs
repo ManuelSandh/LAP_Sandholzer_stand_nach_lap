78ca8d72d529c318d2454174348ddf4ad759bae4
Manuel Sandholzer <htmanus@gmx.at>
2017-04-05 17:25:53 +0200

    tests

--- CardGame.Web/Models/Register.cs
+++ CardGame.Web/Models/Register.cs
@@ -7,7 +7,10 @@ using System.Web;
 namespace CardGame.Web.Models
 {
     public class Register : User
-    {                
+    {
+        
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Confirm password doesn't match, Type again!")]
         public string confirmPassword { get; set; }
     }
 }
--- CardGame.Web/Models/User.cs
+++ CardGame.Web/Models/User.cs
@@ -9,15 +9,16 @@ namespace CardGame.Web.Models
     public class User
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Please enter your firstname!")]
         public string Firstname { get; set; }
+        [Required(ErrorMessage = "Please enter your lastname!")]
         public string Lastname { get; set; }
-        [Required(ErrorMessage = "Email muss angegeben werden")]
+        [Required(ErrorMessage = "Please enter your Email!")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Bitte geben Sie einen Gamertag an")]
+        [Required(ErrorMessage = "Create a Gamertag!")]
         public string Gamertag { get; set; }
-
-        [Required(ErrorMessage = "Kennwort muss angegeben werden")]
+        [Required(ErrorMessage = "Please enter your Password!")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
         public string Salt { get; set; }
